f36f0f1233cd798d5409488dfba8b385f504e67e
Your Name <your.email@example.com>
2024-07-13 21:01:27 -0900

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,57 @@
+let words = document.querySelectorAll(".word");
+
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.appendChild(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxCurrentWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxCurrentWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+
+    currentWordIndex = currentWordIndex === maxCurrentWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText, 3000);
+
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const navList = document.querySelector('.navlist');
+    navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+});
+
+    setInterval(changeWord, 2000);
+
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.querySelector('.navlist');
+
+    menuIcon.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+
